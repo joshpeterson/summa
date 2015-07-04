3f1f5609c9d15d3819063d7739c32bf12850d205
Josh Peterson <petersonjm1@gmail.com>
2015-07-04 08:34:42 -0400

    Remove the default welcome ontroller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'summa/index'
 
-  get 'welcome/index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
