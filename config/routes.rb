e03e4781d1e8157dcf627b71b7ae53d2d34c7e6e
Josh Peterson <petersonjm1@gmail.com>
2015-07-06 05:46:28 -0400

    Add a route to get a part by id.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'summa#index'
 
+  resources :parts, :only => [:show]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
---
+++ test/controllers/parts_controller_route_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class PartsControllerRouteTest < ActionController::TestCase
+  def setup
+    @controller = PartsController.new
+  end
+
+  test "routes the part id to the show action" do
+    assert_recognizes({:controller => "parts", :action => "show", :id => "1"},
+                      "/parts/1")
+  end
+end
