420b39c6449efc1ad6c4549f9916f30ce2a60a18
Josh Peterson <petersonjm1@gmail.com>
2015-07-06 06:06:44 -0400

    Add the start of a part controller.

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,4 +1,12 @@
 class PartsController < ApplicationController
   def show
+    @part = get_part
+    if (@part == nil)
+      render :status => 404
+    end
+  end
+
+  def get_part
+    params.has_key?(:id) ? Part.find(params[:id]) : nil
   end
 end
--- test/controllers/parts_controller_test.rb
+++ test/controllers/parts_controller_test.rb
@@ -2,8 +2,22 @@ require 'test_helper'
 
 class PartsControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
-    assert_response :success
+    get(:show, :id => "1")
+    assert_response(:success)
   end
 
+  test "should return not found for show with out id" do
+    get(:show)
+    assert_response(:not_found)
+  end
+
+  test "should return first part" do
+    get(:show, :id => "1")
+    assert_equal("FIRST PART", @controller.get_part.title)
+  end
+
+  test "should return second part" do
+    get(:show, :id => "2")
+    assert_equal("SECOND PART", @controller.get_part.title)
+  end
 end
