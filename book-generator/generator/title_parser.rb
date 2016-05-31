5fac62bb9d4bb399849a465847c79bdae9a01da0
Josh Peterson <petersonjm1@gmail.com>
2016-05-31 06:29:59 -0400

    Add a title parser
    This title parser was taken mostly from the Rails application. It is
    needed to strip out the undesired characters from the various titles.

---
+++ book-generator/generator/title_parser.rb
@@ -0,0 +1,20 @@
+class TitleParser
+  def strip_parantheses(title)
+    # 1. Replace anything in parantheses with nothing
+    # 2. Replace anything in square brackets with nothing
+    # 3. Replace two or more adjacent spaces with one space
+    title.gsub(/\(.*?\)/, "").gsub(/\[.*?\]/, "").gsub(/\s{2,}/, " ").strip()
+  end
+
+  def title_casing_for(title)
+    title.split.map(&:capitalize).join(" ")
+  end
+
+  def strip_asterisk(title)
+    title.gsub("*", "")
+  end
+
+  def format_title(title)
+    title_casing_for(strip_asterisk(strip_parantheses(title)))
+  end
+end
---
+++ book-generator/test/test_title_parser.rb
@@ -0,0 +1,32 @@
+gem "minitest"
+require "minitest/autorun"
+require_relative "../generator/title_parser"
+
+class TitleParserTest < MiniTest::Test
+  def setup
+    @parser = TitleParser.new
+  end
+  def test_removes_trailing_parantheses
+    assert_equal("FIRST PART", @parser.strip_parantheses("FIRST PART (QQ[1] - 15)"))
+  end
+
+  def test_removes_embedded_parantheses
+    assert_equal("FIRST PART", @parser.strip_parantheses("FIRST (FP) PART"))
+  end
+
+  def test_removes_embedded_bracket
+    assert_equal("FIRST PART", @parser.strip_parantheses("FIRST [1] PART"))
+  end
+
+  def test_changes_the_casing_to_title_casing
+    assert_equal("First Part", @parser.title_casing_for("FIRST PART"))
+  end
+
+  def test_formats_a_title_correctly
+    assert_equal("First Part", @parser.format_title("FIRST (FP) PART* (QQ[1] - 15)"))
+  end
+
+  def test_removes_an_embedded_asterisk
+    assert_equal("OF HONESTY [Test]", @parser.strip_asterisk("OF HONESTY* [Test]"))
+  end
+end
