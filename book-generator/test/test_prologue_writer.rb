f5a247d9fc0f29440a0016fbdf5a85136e66e984
Josh Peterson <petersonjm1@gmail.com>
2016-06-04 07:30:22 -0400

    Make the prologue generator method static.

--- book-generator/test/test_prologue_writer.rb
+++ book-generator/test/test_prologue_writer.rb
@@ -4,17 +4,13 @@ require_relative "custom_assertions.rb"
 require_relative "../generator/prologue_writer"
 
 class TestPrologueWriter < MiniTest::Test
-  def setup
-    @writer = PrologueWriter.new
-  end
-
   def test_prologue_starts_with_title
-    prologue = @writer.write_prologue("Test PART TITle (FOO *)", "")
+    prologue = PrologueWriter.write("Test PART TITle (FOO *)", "")
     assert_startswith("# Test Part Title", prologue)
   end
 
   def test_full_prologue
-    prologue = @writer.write_prologue("Test PART TITle (FOO *)",
+    prologue = PrologueWriter.write("Test PART TITle (FOO *)",
                                       "This is the prologue text.")
     assert_equal($expected_prologue, prologue)
   end
