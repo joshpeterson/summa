d4955216489dd6e63f11016994bd1de05fba2878
Josh Peterson <petersonjm1@gmail.com>
2017-02-14 06:57:27 -0500

    Add lets encrypt verification

---
+++ app/controllers/letsencrypt_controller.rb
@@ -0,0 +1,5 @@
+class LetsencryptController < ApplicationController
+  def verify
+    render text: 'G-SNx8QU3fN95LKlHwWjlPtWUy-WC2s_W4bkVjeCEHs.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   get '/about', to: redirect('/about.html')
   get '/code', to: redirect('/code.html')
 
+  get '/.well-known/acme-challenge/:id' => 'letsencrypt#verify'
+
   # The priority is based upon order of creation: first created -> highest
   # priority.
   # See how all your routes lay out with "rake routes".
