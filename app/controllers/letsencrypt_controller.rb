e070bbd21425513901afa74e3d0afd4c66316afc
Josh Peterson <petersonjm1@gmail.com>
2017-02-15 06:40:33 -0500

    Update the lets encrypt verification key.

--- app/controllers/letsencrypt_controller.rb
+++ app/controllers/letsencrypt_controller.rb
@@ -1,5 +1,5 @@
 class LetsencryptController < ApplicationController
   def verify
-    render text: 'G-SNx8QU3fN95LKlHwWjlPtWUy-WC2s_W4bkVjeCEHs.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
+    render text: '-Pk_eaIGfnFWuM5fv8n3t9kFCbM-EfjA-IqPO_PfVfE.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
   end
 end
