99b80e2201502b3dbce6dfab918572344ab39438
Josh Peterson <petersonjm1@gmail.com>
2017-02-17 06:55:32 -0500

    More letsencrypt verification.

--- app/controllers/letsencrypt_controller.rb
+++ app/controllers/letsencrypt_controller.rb
@@ -1,7 +1,11 @@
 class LetsencryptController < ApplicationController
   def verify
     if params[:id] == '-Pk_eaIGfnFWuM5fv8n3t9kFCbM-EfjA-IqPO_PfVfE'
+      # www.summexplorer.com
       render text: '-Pk_eaIGfnFWuM5fv8n3t9kFCbM-EfjA-IqPO_PfVfE.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
+    elsif params[:id] == 'I-OtiJzIqHtCb8BTNxv-HpUaaFprC8Y_MoFNADbXpqI'
+      # summaexplorer.com
+      render text: 'I-OtiJzIqHtCb8BTNxv-HpUaaFprC8Y_MoFNADbXpqI.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
     end
   end
 end
