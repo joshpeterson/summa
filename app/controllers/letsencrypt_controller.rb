fb9c2e90d5cefe4a5075de8ce6a652d610398081
Josh Peterson <petersonjm1@gmail.com>
2017-02-17 06:44:52 -0500

    Use a new letsencrypt key.

--- app/controllers/letsencrypt_controller.rb
+++ app/controllers/letsencrypt_controller.rb
@@ -1,5 +1,7 @@
 class LetsencryptController < ApplicationController
   def verify
-    render text: '-Pk_eaIGfnFWuM5fv8n3t9kFCbM-EfjA-IqPO_PfVfE.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
+    if params[:id] == '-Pk_eaIGfnFWuM5fv8n3t9kFCbM-EfjA-IqPO_PfVfE'
+      render text: '-Pk_eaIGfnFWuM5fv8n3t9kFCbM-EfjA-IqPO_PfVfE.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
+    end
   end
 end
