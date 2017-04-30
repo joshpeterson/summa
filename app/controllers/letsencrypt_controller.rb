394ba26841308cc3bcc75e0be004089db8431dee
Josh Peterson <petersonjm1@gmail.com>
2017-04-30 08:18:58 -0400

    Update letsencrypt id for renewal

--- app/controllers/letsencrypt_controller.rb
+++ app/controllers/letsencrypt_controller.rb
@@ -1,11 +1,8 @@
 class LetsencryptController < ApplicationController
   def verify
-    if params[:id] == '-Pk_eaIGfnFWuM5fv8n3t9kFCbM-EfjA-IqPO_PfVfE'
+    if params[:id] == 'EWNb8sJjiJhgVnljjSN1iKfKogi-svHjTL_ZfW54ak0'
       # www.summexplorer.com
-      render text: '-Pk_eaIGfnFWuM5fv8n3t9kFCbM-EfjA-IqPO_PfVfE.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
-    elsif params[:id] == 'I-OtiJzIqHtCb8BTNxv-HpUaaFprC8Y_MoFNADbXpqI'
-      # summaexplorer.com
-      render text: 'I-OtiJzIqHtCb8BTNxv-HpUaaFprC8Y_MoFNADbXpqI.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
+      render text: 'EWNb8sJjiJhgVnljjSN1iKfKogi-svHjTL_ZfW54ak0.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
     end
   end
 end
