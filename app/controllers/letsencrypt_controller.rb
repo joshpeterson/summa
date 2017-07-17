5eed621dee7084f2a32c63a71a62b6cdb1510c39
Josh Peterson <petersonjm1@gmail.com>
2017-07-17 06:34:20 -0400

    Renew lets encrypt certificate.

--- app/controllers/letsencrypt_controller.rb
+++ app/controllers/letsencrypt_controller.rb
@@ -1,8 +1,8 @@
 class LetsencryptController < ApplicationController
   def verify
-    if params[:id] == 'EWNb8sJjiJhgVnljjSN1iKfKogi-svHjTL_ZfW54ak0'
+    if params[:id] =='epZ8pqgvBJ1B2JIZFDgN1m7x1C0ecP4UrmJloTgL9hI'
       # www.summexplorer.com
-      render text: 'EWNb8sJjiJhgVnljjSN1iKfKogi-svHjTL_ZfW54ak0.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
+      render text: 'epZ8pqgvBJ1B2JIZFDgN1m7x1C0ecP4UrmJloTgL9hI.HGKAemL2fUjIRadTuQf-sqPIqvo2JUZKqjZzg1u2D0U'
     end
   end
 end
