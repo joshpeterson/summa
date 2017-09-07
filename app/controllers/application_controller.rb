a6286fa93a0fcd97c1ab4237668f21309b0e576f
Josh Peterson <petersonjm1@gmail.com>
2017-09-07 06:51:31 -0400

    Enable the rack mini profiler in production
    * Use the ?rmp=1 query parameter to enable it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :check_rack_mini_profiler
+  def check_rack_mini_profiler
+    Rack::MiniProfiler.authorize_request if params[:rmp]
+  end
 end
