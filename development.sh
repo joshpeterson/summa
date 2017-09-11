f11537e8c8abb2ebdf93b741fec5e3ea49e72a63
Josh Peterson <petersonjm1@gmail.com>
2017-09-11 06:34:56 -0400

    Use bundler to run rails in setup scripts

--- development.sh
+++ development.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rails server -b 0.0.0.0
+bundle exec rails server -b 0.0.0.0
--- production.sh
+++ production.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-RAILS_ENV=production rake assets:precompile
-RAILS_ENV=production SECRET_KEY_BASE=test OPENSHIFT_APP_NAME=database_development RAILS_SERVE_STATIC_FILES=1 rails server -b 0.0.0.0
+RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production RAILS_SERVE_STATIC_FILES=1 bundle exec rails server -b 0.0.0.0
