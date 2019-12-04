ef6904a46d71bb66fb026f946f5e8048a5f5d45d
Josh Peterson <petersonjm1@gmail.com>
2019-12-04 06:32:45 -0500

    Restore routes removed by rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'outline/show'
+
+  get 'reader/show'
+
+  get 'articles/show'
+
+  get 'questions/show'
+
+  get 'treatises/show'
+
+  get 'parts/show'
+
+  get 'browse/', to: 'summa#show'
+  get 'read/', to: 'reader#show'
+  get 'outline/', to: 'outline#show'
+
+  get '/about' => 'static_pages#about'
+  get '/' => 'static_pages#home'
+
+  get '/.well-known/acme-challenge/:id' => 'letsencrypt#verify'
+
+  resources :parts, only: [:show]
+  resources :treatises, only: [:show]
+  resources :questions, only: [:show]
+  resources :articles, only: [:show]
 end
